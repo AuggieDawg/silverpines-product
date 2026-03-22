a4f1f8576da82161b7611b08586926afa248836f
Agustin Alberto Rosas Franco <agustin03036944@gmail.com>
2026-03-21 21:19:26 -0600

    Production Testing

--- app/page.tsx
+++ app/page.tsx
@@ -14,3 +14,10 @@ export default function HomePage() {
     </main>
   );
 }
+
+
+
+<footer className="text-xs opacity-70">
+  {process.env.VERCEL_TARGET_ENV} · {process.env.VERCEL_GIT_COMMIT_REF} ·{" "}
+  {process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7)}
+</footer>
